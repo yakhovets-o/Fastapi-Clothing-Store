17014a83d2d68103def5eac2a78067c58feca109
yakhovets-o <yakhovetso@gmail.com>
2024-06-28 17:24:33 +0300

    create base model

---
+++ src/models/base.py
@@ -0,0 +1,20 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import declared_attr
+
+from src.configurations import settings
+from src.utils import camel_case_to_snake_case
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention
+
+    )
+
+    @classmethod
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return camel_case_to_snake_case(cls.__name__)
