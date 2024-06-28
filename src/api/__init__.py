25281097ea1803e35c1c1fd2733b8bf3abbadb3b
yakhovets-o <yakhovetso@gmail.com>
2024-06-28 13:41:56 +0300

    imports

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,5 +1,8 @@
-from src.api.clothing import router as clothing_router
+from fastapi import APIRouter
 
+from src.configurations import settings
+
+from .api_v1 import router as router_api_v1
 
 def include_router(app):
     app.include_router(clothing_router)
