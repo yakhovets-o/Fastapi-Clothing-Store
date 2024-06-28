8f5bb074b6ad70805a12cf0c4104f14059772ec6
yakhovets-o <yakhovetso@gmail.com>
2024-06-28 15:42:55 +0300

    create model_config

--- src/configurations/__init__.py
+++ src/configurations/__init__.py
@@ -10,6 +10,13 @@ from .database import DatabaseConfig
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=('.env',),
+        case_sensitive=False,
+        env_nested_delimiter='__',
+        env_prefix='FASTAPI__'
+
+    )
     run: RunAppConfig = RunAppConfig()
     fastapi: FastApiConfig = FastApiConfig()
     api: ApiConfig = ApiConfig()
