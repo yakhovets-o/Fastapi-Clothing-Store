a1965d2da5ff4b4953bd1261255a12b4fed4b877
yakhovets-o <yakhovetso@gmail.com>
2024-06-28 13:41:10 +0300

    create __init__.py src\api\api_v1

---
+++ src/api/api_v1/__init__.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from src.configurations import settings
+
+from .clothing import router as clothing_router
+
+router = APIRouter(
+    prefix=settings.api.version.v1,
+)
+
+router.include_router(
+    router=clothing_router,
+    prefix=settings.api.prefix.clothing
+)
