82aabef966b3a2bf02d54bb9e939a4353b0bdce5
yakhovets-o <yakhovetso@gmail.com>
2024-06-28 13:44:15 +0300

    create app.py config

---
+++ src/configurations/app.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+
+
+class RunAppConfig(BaseModel):
+    host: str = '127.0.0.1'
+    port: int = 8000
+
+
+class FastApiConfig(BaseModel):
+    title: str = 'Clothing Store'
+    description: str = 'Realization of vintage clothes'
+
+
+class ApiVVersion(BaseModel):
+    v1: str = '/v1'
+
+
+class ApiPrefix(BaseModel):
+    clothing: str = '/clothing'
+    footwear: str = '/footwear'
+    accessories: str = '/accessories'
+
+
+class ApiTags(BaseModel):
+    clothing: list = ['Clothing']
+    footwear: list = ['Footwear']
+    accessories: list = ['Accessories']
+
+
+class ApiConfig(BaseModel):
+    main_prefix: str = '/api'
+    version: ApiVVersion = ApiVVersion()
+    tags: ApiTags = ApiTags()
+    prefix: ApiPrefix = ApiPrefix()
