8c827d718571bd89baeb072bb0f312bc98324a33
yakhovets-o <yakhovetso@gmail.com>
2024-06-30 14:03:22 +0300

    create init import SizeInternational, SizeEU, SizeAccessories

---
+++ src/utils/custom_data_types/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    'SizeClothing',
+    'SizeAccessories',
+    'SizeFootwear'
+
+)
+
+from .clothing import SizeInternational as SizeClothing
+from .footwear import SizeEU as SizeFootwear
+from .accessories import SizeAccessories
