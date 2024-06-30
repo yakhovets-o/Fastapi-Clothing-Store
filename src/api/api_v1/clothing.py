e882b729da36ba1e5f67561d412b234529ae5716
yakhovets-o <yakhovetso@gmail.com>
2024-06-30 14:13:00 +0300

    refactor clothing

--- src/api/api_v1/clothing.py
+++ src/api/api_v1/clothing.py
@@ -1,18 +1,19 @@
 from fastapi import APIRouter
 import uuid
 
-from src.schemas.clothing import ClothingSchemaAddEdit
+
+from src.schemas.clothing import ClothingSchema
 
 router = APIRouter()
 
 fake_clothing = [
-    {'brand': 'Nike', 'name': 'vintage', 'size': 'L', 'price': 50,
+    {'id': str(uuid.uuid4()), 'brand': 'Nike', 'name': 'vintage', 'size': 'L', 'price': 50,
      'description': 'крутая кофта'},
-    {'brand': 'Adidas', 'name': 'new', 'size': 'S', 'price': 110,
+    {'id': str(uuid.uuid4()), 'brand': 'Adidas', 'name': 'new', 'size': 'S', 'price': 110,
      'description': 'крутая майка'}
 ]
 
 
-@router.get('/', response_model=list[ClothingSchemaAddEdit])
+@router.get('/', response_model=list[ClothingSchema])
 async def get_clothing():
     return [i for i in fake_clothing]
