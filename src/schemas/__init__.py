6a02c6f6cde9b08264d25244050098db69931030
yakhovets-o <yakhovetso@gmail.com>
2024-07-01 16:15:25 +0300

    create init schemas

---
+++ src/schemas/__init__.py
@@ -0,0 +1,13 @@
+__all__ = (
+    'AccessoriesSchema',
+    'AccessoriesSchemaAddEdit',
+    'FootwearSchema',
+    'FootwearSchemaAddEdit',
+    'ClothingSchema',
+    'ClothingSchemaAddEdit'
+
+)
+
+from .accessories import AccessoriesSchema, AccessoriesSchemaAddEdit
+from .footwear import FootwearSchema, FootwearSchemaAddEdit
+from .clothing import ClothingSchema, ClothingSchemaAddEdit
