b42723613df44008c3096c183aa2e9aa331cd2c0
yakhovets-o <yakhovetso@gmail.com>
2024-07-01 16:20:37 +0300

    refactor footwear model

--- src/models/footwear.py
+++ src/models/footwear.py
@@ -1,17 +1,15 @@
-from datetime import datetime, timezone
-
 from sqlalchemy.orm import Mapped
 
 from src.utils.custom_data_types import SizeFootwear
 
 from .base import Base
 from .mixins.uuid_pk import UuidPkMixin
+from .mixins.updated_created_at import UpdatedCreatedAtMixin
 
 
-class Footwear(UuidPkMixin, Base):
+class Footwear(UuidPkMixin, UpdatedCreatedAtMixin, Base):
     brand: Mapped[str]
     name: Mapped[str]
     size: Mapped[SizeFootwear]
     price: Mapped[int]
     description: Mapped[str]
-    create_at: Mapped[datetime] = datetime.now(timezone.utc)
