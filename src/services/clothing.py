50fd4410345eb02597985388b3a9fc3001e88253
yakhovets-o <yakhovetso@gmail.com>
2024-07-02 00:25:36 +0300

    add crud in clothing services

--- src/services/clothing.py
+++ src/services/clothing.py
@@ -10,8 +10,20 @@ from src.models import Clothing
 
 
 class ClothingService:
-    def __init__(self, session: AsyncSession):
-        self.clothing_repository = ClothingRepository(session=session)
+    def __init__(self, session: AsyncSession) -> None:
+        self.clothing_repository: ClothingRepository = ClothingRepository(session=session)
 
-    async def add_clothing(self, clothing: ClothingSchemaAddEdit):
-        return await self.clothing_repository.add_clothing(new_clothing=clothing)
+    async def get_all_(self) -> Sequence[ClothingSchema]:
+        return await self.clothing_repository.get_all()
+
+    async def get_by_id(self, _id: UUID4) -> Clothing:
+        return await self.clothing_repository.get_by_id(_id=_id)
+
+    async def create(self, clothing: ClothingSchemaAddEdit) -> Clothing:
+        return await self.clothing_repository.create(new_clothing=clothing)
+
+    async def update(self, _id: UUID4, clothing: ClothingSchemaAddEdit) -> None:
+        return await self.clothing_repository.update(_id=_id, update_clothing=clothing)
+
+    async def delete(self, _id: UUID4) -> None:
+        return await self.clothing_repository.delete(_id=id)
