a47dce4bd92af557e41ecbc67235f7c3237b1a69
yakhovets-o <yakhovetso@gmail.com>
2024-07-01 16:16:10 +0300

    import UpdatedCreatedAtMixin

--- src/models/accessories.py
+++ src/models/accessories.py
@@ -4,12 +4,12 @@ from sqlalchemy.orm import Mapped
 
 from src.utils.custom_data_types import SizeAccessories
 
-
 from .base import Base
 from .mixins.uuid_pk import UuidPkMixin
+from .mixins.updated_created_at import UpdatedCreatedAtMixin
 
 
-class Accessories(UuidPkMixin, Base):
+class Accessories(UuidPkMixin, UpdatedCreatedAtMixin, Base):
     brand: Mapped[str]
     name: Mapped[str]
     size: Mapped[SizeAccessories]
