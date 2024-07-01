0aece710502f5f5e876ee6bcd96aeeb439e9bf9f
yakhovets-o <yakhovetso@gmail.com>
2024-07-02 00:27:09 +0300

    add  crud in clothing  repo

--- src/repository/clothing.py
+++ src/repository/clothing.py
@@ -13,9 +13,34 @@ class ClothingRepository:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    async def add_clothing(self, new_clothing: ClothingSchemaAddEdit) -> Clothing:
+    async def get_all(self) -> Sequence[ClothingSchema]:
+        stmt = select(Clothing)
+        result = await self.session.execute(stmt)
+        clothing_models = result.scalars().all()
+        return [ClothingSchema.model_validate(clothing_model) for clothing_model in clothing_models]
+
+    async def get_by_id(self, _id: UUID4) -> Clothing:
+        print(_id)
+        stmt = select(Clothing).where(str(Clothing.id) == _id)
+        clothing: Result = await self.session.execute(stmt)
+
+        return clothing.scalar()
+
+    async def create(self, new_clothing: ClothingSchemaAddEdit) -> ClothingSchemaAddEdit:
         clothing = Clothing(**new_clothing.model_dump())
         self.session.add(clothing)
         await self.session.commit()
-        return clothing
+        return ClothingSchemaAddEdit.model_validate(clothing)
 
+    async def update(self, _id: UUID4, update_clothing: ClothingSchemaAddEdit) -> None:
+        clothing = update(Clothing).where(str(Clothing.id) == _id).values(**update_clothing.model_dump())
+
+        await self.session.execute(clothing)
+        await self.session.commit()
+        await self.session.refresh(clothing)
+
+    async def delete(self, _id: UUID4) -> None:
+        clothing = delete(Clothing).where(str(Clothing.id) == _id)
+
+        await self.session.execute(clothing)
+        await self.session.commit()
