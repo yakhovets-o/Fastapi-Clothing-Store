e5a8dd787a4f1027c167b94bca4caeb20aeefdf5
yakhovets-o <yakhovetso@gmail.com>
2024-07-01 16:20:12 +0300

    refactor clothing model

--- src/models/clothing.py
+++ src/models/clothing.py
@@ -1,17 +1,14 @@
-from datetime import datetime, timezone
-
 from sqlalchemy.orm import Mapped
-
 from src.utils.custom_data_types import SizeClothing
 
 from .base import Base
 from .mixins.uuid_pk import UuidPkMixin
+from .mixins.updated_created_at import UpdatedCreatedAtMixin
 
 
-class Clothing(UuidPkMixin, Base):
+class Clothing(UuidPkMixin, Base, UpdatedCreatedAtMixin):
     brand: Mapped[str]
     name: Mapped[str]
     size: Mapped[SizeClothing]
     price: Mapped[int]
     description: Mapped[str]
-    create_at: Mapped[datetime] = datetime.now(timezone.utc)
