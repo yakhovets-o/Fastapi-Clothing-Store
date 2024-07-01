e5781c24842c04b502da33a956ae371cb1a3bd7d
yakhovets-o <yakhovetso@gmail.com>
2024-07-02 00:24:41 +0300

    import Sequence, UUID4, Clothing, ClothingSchemaAddEdit, ClothingSchema

--- src/services/clothing.py
+++ src/services/clothing.py
@@ -1,7 +1,12 @@
+from typing import Sequence
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from pydantic import UUID4
+
 from src.repository.clothing import ClothingRepository
-from src.schemas import ClothingSchemaAddEdit
+from src.schemas import ClothingSchemaAddEdit, ClothingSchema
+from src.models import Clothing
 
 
 class ClothingService:
