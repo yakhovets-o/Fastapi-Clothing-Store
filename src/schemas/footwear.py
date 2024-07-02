4dda54178affba280b55a83c80c33e92ad75782f
yakhovets-o <yakhovetso@gmail.com>
2024-07-02 15:57:59 +0300

    refactor footwear schemas

--- src/schemas/footwear.py
+++ src/schemas/footwear.py
@@ -1,26 +1,21 @@
 from datetime import datetime
 from pydantic import BaseModel, Field, UUID4
 
-from src.utils.custom_data_types import SizeFootwear
+from src.enums import SizeFootwear
 
 
-class FootwearSchema(BaseModel):
-    id: UUID4
+class FootwearSchemaCRUD(BaseModel):
     brand: str
     name: str
     size: SizeFootwear
     price: int = Field(ge=0)
     description: str
+
+
+class FootwearSchemaORM(FootwearSchemaCRUD):
+    id: UUID4
     create_at: datetime
     updated_at: datetime
 
     class Config:
         from_attributes = True
-
-
-class FootwearSchemaAddEdit(BaseModel):
-    brand: str
-    name: str
-    size: SizeFootwear
-    price: int = Field(ge=0)
-    description: str
