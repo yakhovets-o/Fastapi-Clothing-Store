5b4abbe7efd0fcbf407839436b9e5252f1280495
yakhovets-o <yakhovetso@gmail.com>
2024-07-02 20:18:28 +0300

    create accessories api v1

---
+++ src/api/api_v1/accessories.py
@@ -0,0 +1,62 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Path
+from fastapi import Depends
+
+from fastapi_pagination import add_pagination
+from fastapi_pagination.links import LimitOffsetPage
+
+from pydantic import UUID4
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.schemas.accessories import AccessoriesSchemaCRUD
+from src.services.accessories import AccessoriesService
+from src.bd import db_helper
+
+router = APIRouter()
+
+
+@router.get('/', response_model=LimitOffsetPage[AccessoriesSchemaCRUD])
+async def get_all_accessories(session: Annotated[AsyncSession, Depends(db_helper.session_getter)]):
+    get_all = AccessoriesService(session=session)
+    return await get_all.get_all_()
+
+
+add_pagination(router)
+
+
+@router.get('/{id}/', response_model=AccessoriesSchemaCRUD)
+async def get_accessories_by_id(
+        accessories_id: Annotated[UUID4, Path(alias='id')],
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)]):
+    get_accessories = AccessoriesService(session=session)
+    return await get_accessories.get_by_id(_id=accessories_id)
+
+
+@router.post('/', response_model=AccessoriesSchemaCRUD)
+async def create_accessories(
+        accessories: AccessoriesSchemaCRUD,
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)]):
+    new_accessories = AccessoriesService(session=session)
+    return await new_accessories.create(accessories=accessories)
+
+
+@router.patch('/{id}/')
+async def update_accessories(
+        accessories_id: Annotated[UUID4, Path(alias='id')],
+        accessories: AccessoriesSchemaCRUD,
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)]):
+    update_accessories = AccessoriesService(session=session)
+    if await update_accessories.update(_id=accessories_id, accessories=accessories):
+        return {"message": "ok"}
+
+
+@router.delete('/{id}/')
+async def delete_accessories(
+        accessories_id: Annotated[UUID4, Path(alias='id')],
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)]):
+    print(accessories_id)
+    delete_accessories = AccessoriesService(session=session)
+
+    return await delete_accessories.delete(_id=accessories_id)
