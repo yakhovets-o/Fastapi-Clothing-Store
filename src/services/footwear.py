cceeef9d0ee34d8419d14756d3346294f415c9b3
yakhovets-o <yakhovetso@gmail.com>
2024-07-02 20:11:50 +0300

    create footwear services

---
+++ src/services/footwear.py
@@ -0,0 +1,28 @@
+from typing import Sequence
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from pydantic import UUID4
+
+from src.repository.footwear import FootwearRepository
+from src.schemas import FootwearSchemaCRUD, FootwearSchemaORM
+
+
+class FootwearService:
+    def __init__(self, session: AsyncSession) -> None:
+        self.footwear_repository: FootwearRepository = FootwearRepository(session=session)
+
+    async def get_all_(self) -> Sequence[FootwearSchemaORM]:
+        return await self.footwear_repository.get_all()
+
+    async def get_by_id(self, _id: UUID4) -> FootwearSchemaORM:
+        return await self.footwear_repository.get_by_id(_id=_id)
+
+    async def create(self, footwear: FootwearSchemaCRUD) -> FootwearSchemaORM:
+        return await self.footwear_repository.create(new_footwear=footwear)
+
+    async def update(self, _id: UUID4, footwear: FootwearSchemaCRUD) -> None:
+        return await self.footwear_repository.update(_id=_id, update_footwear=footwear)
+
+    async def delete(self, _id: UUID4) -> None:
+        return await self.footwear_repository.delete(_id=_id)
