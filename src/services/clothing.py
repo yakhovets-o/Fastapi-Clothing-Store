a8c22551744a4189c345633640a914a62cd3bfa8
yakhovets-o <yakhovetso@gmail.com>
2024-07-02 13:39:58 +0300

    refactor clothing services

--- src/services/clothing.py
+++ src/services/clothing.py
@@ -16,14 +16,14 @@ class ClothingService:
     async def get_all_(self) -> Sequence[ClothingSchema]:
         return await self.clothing_repository.get_all()
 
-    async def get_by_id(self, _id: UUID4) -> Clothing:
+    async def get_by_id(self, _id: UUID4) -> ClothingSchema:
         return await self.clothing_repository.get_by_id(_id=_id)
 
-    async def create(self, clothing: ClothingSchemaAddEdit) -> Clothing:
+    async def create(self, clothing: ClothingSchemaAddEdit) -> ClothingSchema:
         return await self.clothing_repository.create(new_clothing=clothing)
 
     async def update(self, _id: UUID4, clothing: ClothingSchemaAddEdit) -> None:
         return await self.clothing_repository.update(_id=_id, update_clothing=clothing)
 
     async def delete(self, _id: UUID4) -> None:
-        return await self.clothing_repository.delete(_id=id)
+        return await self.clothing_repository.delete(_id=_id)
