721089256b745327d48f75982e56647f20ac6f86
yakhovets-o <yakhovetso@gmail.com>
2024-07-02 20:18:05 +0300

    refactor accessories model

--- src/models/accessories.py
+++ src/models/accessories.py
@@ -7,10 +7,9 @@ from .mixins.uuid_pk import UuidPkMixin
 from .mixins.updated_created_at import UpdatedCreatedAtMixin
 
 
-class Accessories(UuidPkMixin, UpdatedCreatedAtMixin, Base):
+class Accessories(UuidPkMixin, Base, UpdatedCreatedAtMixin):
     brand: Mapped[str]
     name: Mapped[str]
     size: Mapped[SizeAccessories]
     price: Mapped[int]
     description: Mapped[str]
-
