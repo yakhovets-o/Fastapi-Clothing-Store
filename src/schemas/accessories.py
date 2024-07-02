4bf29c1e2128513bdb3ace5afca4385530d6198b
yakhovets-o <yakhovetso@gmail.com>
2024-07-02 16:01:44 +0300

    refactor   accessories schemas

--- src/schemas/accessories.py
+++ src/schemas/accessories.py
@@ -2,26 +2,21 @@ from datetime import datetime
 
 from pydantic import BaseModel, Field, UUID4
 
-from src.utils.custom_data_types import SizeAccessories
+from src.enums import SizeAccessories
 
 
-class AccessoriesSchema(BaseModel):
-    id: UUID4
+class AccessoriesSchemaCRUD(BaseModel):
     brand: str
     name: str
     size: SizeAccessories
     price: int = Field(ge=0)
     description: str
+
+
+class AccessoriesSchemaORM(AccessoriesSchemaCRUD):
+    id: UUID4
     create_at: datetime
     updated_at: datetime
 
     class Config:
         from_attributes = True
-
-
-class AccessoriesSchemaAddEdit(BaseModel):
-    brand: str
-    name: str
-    size: SizeAccessories
-    price: int = Field(ge=0)
-    description: str
