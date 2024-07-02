c5dd2aa00f5b5d95a23878d01835e9ddefff5703
yakhovets-o <yakhovetso@gmail.com>
2024-07-02 16:02:34 +0300

    refactor   import

---
+++ src/enums/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    'SizeClothing',
+    'SizeAccessories',
+    'SizeFootwear'
+
+)
+
+from .clothing_size import SizeInternational as SizeClothing
+from .footwear_size import SizeEU as SizeFootwear
+from .accessories_size import SizeAccessories
