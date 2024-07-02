79044c252fd83b8ccf7fddf1578e1d18bed071a0
yakhovets-o <yakhovetso@gmail.com>
2024-07-02 15:59:44 +0300

    delete src.models  Clothing and refactor import ClothingSchemaCRUD, ClothingSchemaORM

--- src/services/clothing.py
+++ src/services/clothing.py
@@ -5,24 +5,23 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from pydantic import UUID4
 
 from src.repository.clothing import ClothingRepository
-from src.schemas import ClothingSchemaAddEdit, ClothingSchema
-from src.models import Clothing
+from src.schemas import ClothingSchemaCRUD, ClothingSchemaORM
 
 
 class ClothingService:
     def __init__(self, session: AsyncSession) -> None:
         self.clothing_repository: ClothingRepository = ClothingRepository(session=session)
 
-    async def get_all_(self) -> Sequence[ClothingSchema]:
+    async def get_all_(self) -> Sequence[ClothingSchemaORM]:
         return await self.clothing_repository.get_all()
 
-    async def get_by_id(self, _id: UUID4) -> ClothingSchema:
+    async def get_by_id(self, _id: UUID4) -> ClothingSchemaORM:
         return await self.clothing_repository.get_by_id(_id=_id)
 
-    async def create(self, clothing: ClothingSchemaAddEdit) -> ClothingSchema:
+    async def create(self, clothing: ClothingSchemaCRUD) -> ClothingSchemaORM:
         return await self.clothing_repository.create(new_clothing=clothing)
 
-    async def update(self, _id: UUID4, clothing: ClothingSchemaAddEdit) -> None:
+    async def update(self, _id: UUID4, clothing: ClothingSchemaCRUD) -> None:
         return await self.clothing_repository.update(_id=_id, update_clothing=clothing)
 
     async def delete(self, _id: UUID4) -> None:
