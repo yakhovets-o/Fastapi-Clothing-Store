8b8ba39839a8fad73935402da07c41d2137fd309
yakhovets-o <yakhovetso@gmail.com>
2024-07-02 16:00:46 +0300

    refactor import

--- src/repository/clothing.py
+++ src/repository/clothing.py
@@ -6,32 +6,32 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from pydantic import UUID4
 
 from src.models import Clothing
-from src.schemas import ClothingSchemaAddEdit, ClothingSchema
+from src.schemas import ClothingSchemaCRUD, ClothingSchemaORM
 
 
 class ClothingRepository:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    async def get_all(self) -> Sequence[ClothingSchema]:
+    async def get_all(self) -> Sequence[ClothingSchemaORM]:
         stmt = select(Clothing)
         result = await self.session.execute(stmt)
         clothing_models = result.scalars().all()
-        return [ClothingSchema.model_validate(clothing_model) for clothing_model in clothing_models]
+        return [ClothingSchemaORM.model_validate(clothing_model) for clothing_model in clothing_models]
 
-    async def get_by_id(self, _id: UUID4) -> ClothingSchema:
+    async def get_by_id(self, _id: UUID4) -> ClothingSchemaORM:
         stmt = select(Clothing).where(Clothing.id == _id)
         result: Result = await self.session.execute(stmt)
         clothing_models = result.scalar()
-        return ClothingSchema.model_validate(clothing_models)
+        return ClothingSchemaORM.model_validate(clothing_models)
 
-    async def create(self, new_clothing: ClothingSchemaAddEdit) -> ClothingSchema:
+    async def create(self, new_clothing: ClothingSchemaCRUD) -> ClothingSchemaORM:
         clothing = Clothing(**new_clothing.model_dump())
         self.session.add(clothing)
         await self.session.commit()
-        return ClothingSchema.model_validate(clothing)
+        return ClothingSchemaORM.model_validate(clothing)
 
-    async def update(self, _id: UUID4, update_clothing: ClothingSchemaAddEdit) -> None:
+    async def update(self, _id: UUID4, update_clothing: ClothingSchemaCRUD) -> None:
         stmt = update(Clothing).where(Clothing.id == _id).values(**update_clothing.model_dump())
 
         await self.session.execute(stmt)
