0e8d2ddb8cb41ec3a312717287607049ec0a6189
yakhovets-o <yakhovetso@gmail.com>
2024-07-05 20:41:00 +0300

    refactor  clothing  schemas  ⚒️

--- src/schemas/clothing.py
+++ src/schemas/clothing.py
@@ -9,18 +9,21 @@ from pydantic import (
 from src.enums import SizeClothing
 
 
-class ClothingSchemaCRUD(BaseModel):
+class ClothingSchemaCreate(BaseModel):
     brand: str
     name: str
     size: SizeClothing = Field(default=None, validate_default=True)
     price: int = Field(ge=0)
     description: str
 
+    class Config:
+        from_attributes = True
 
-class ClothingSchemaORM(ClothingSchemaCRUD):
+
+class ClothingSchemaUpdate(ClothingSchemaCreate): ...
+
+
+class ClothingSchemaRead(ClothingSchemaCreate):
     id: UUID4
     create_at: datetime
     updated_at: datetime
-
-    class Config:
-        from_attributes = True
