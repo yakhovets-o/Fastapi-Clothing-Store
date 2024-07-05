1113a2c9756d381b0e65ca049b69372cb4b731a7
yakhovets-o <yakhovetso@gmail.com>
2024-07-05 21:05:21 +0300

    refactor footwear services 🛠

--- src/services/footwear.py
+++ src/services/footwear.py
@@ -1,32 +1,31 @@
-from typing import Sequence
+from typing import Optional
 
 from pydantic import UUID4
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.repository.footwear import FootwearRepository
-from src.schemas import (
-    FootwearSchemaCRUD,
-    FootwearSchemaORM,
-)
+from src.repositories.footwear import FootwearRepository
+from src.schemas import FootwearSchemaCreate, FootwearSchemaRead, FootwearSchemaUpdate
 
 
 class FootwearService:
     def __init__(self, session: AsyncSession) -> None:
         self.footwear_repository: FootwearRepository = FootwearRepository(session=session)
 
-    async def get_all_(self) -> Sequence[FootwearSchemaORM]:
+    async def get_all_(self) -> list[FootwearSchemaRead]:
         return await self.footwear_repository.get_all()
 
-    async def get_by_id(self, _id: UUID4) -> FootwearSchemaORM | None:
-        return await self.footwear_repository.get_by_id(_id=_id)
+    async def get_by_id(self, footwear_id: UUID4) -> Optional[FootwearSchemaRead]:
+        return await self.footwear_repository.get_by_id(footwear_id=footwear_id)
 
-    async def create(self, footwear: FootwearSchemaCRUD) -> FootwearSchemaORM:
+    async def create(self, footwear: FootwearSchemaCreate) -> FootwearSchemaRead:
         return await self.footwear_repository.create(new_footwear=footwear)
 
     async def update(
-        self, _id: UUID4, footwear: FootwearSchemaCRUD
-    ) -> dict[str, str] | None:
-        return await self.footwear_repository.update(_id=_id, update_footwear=footwear)
+        self, footwear_id: UUID4, footwear: FootwearSchemaUpdate
+    ) -> Optional[FootwearSchemaRead]:
+        return await self.footwear_repository.update(
+            footwear_id=footwear_id, update_footwear=footwear
+        )
 
-    async def delete(self, _id: UUID4) -> dict[str, str] | None:
-        return await self.footwear_repository.delete(_id=_id)
+    async def delete(self, footwear_id: UUID4) -> None:
+        return await self.footwear_repository.delete(footwear_id=footwear_id)
