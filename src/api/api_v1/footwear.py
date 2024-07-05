97666d9167596c03beaadaf7f761de5ccbd3c0b5
yakhovets-o <yakhovetso@gmail.com>
2024-07-05 22:30:25 +0300

    status code HTTP_204_NO_CONTENT delete_footwear ❤️‍🩹

--- src/api/api_v1/footwear.py
+++ src/api/api_v1/footwear.py
@@ -74,7 +74,7 @@ async def update_footwear(
         )
 
 
-@router.delete("/{id}/", status_code=status.HTTP_201_CREATED, response_model=None)
+@router.delete("/{id}/", status_code=status.HTTP_204_NO_CONTENT, response_model=None)
 async def delete_footwear(
     footwear_id: Annotated[UUID4, Path(alias="id")],
     service: Annotated[FootwearService, Depends(get_service(FootwearService))],
