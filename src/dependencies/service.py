2b1071bf20fd24f52e3ba043b851c749cd2e95ce
yakhovets-o <yakhovetso@gmail.com>
2024-07-10 17:10:14 +0300

    refactor service 🗡

--- src/dependencies/service.py
+++ src/dependencies/service.py
@@ -1,11 +1,16 @@
 from fastapi import Depends
+from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.dependencies.redis import cache
 from src.helpers import db_helper
 
 
 def get_service(service):
-    def _get_service(session: AsyncSession = Depends(db_helper.session_getter)):
-        return service(session)
+    def _get_service(
+        session: AsyncSession = Depends(db_helper.session_getter),
+        redis_client: Redis = Depends(cache),
+    ):
+        return service(session, redis_client)
 
     return _get_service
